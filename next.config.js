188de59855cc974f88b1e55c3b519fd3d13a89a2
Csabbah <csabbah@ryerson.ca>
2022-09-19 15:22:44 -0400

    Init commit and project setup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['bayut-production.s3.eu-central-1.amazonaws.com'],
+  },
   reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+  // reactStrictMode: true, // We also don't need this this
+  swcMinify: true,
+};
+
+module.exports = nextConfig;
